7aeccd5e0449bbb773df315db43f16a80085af04
suyashmisra <suyash.misra@quovantis.com>
2021-02-10 01:36:02 +0530

    added option json:true to automatically parse response body

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const request = require("request");
 const url =
   "http://api.weatherstack.com/current?access_key=a6339f0a06cad65af649e579b9debaeb&query=37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-  const weatherData = JSON.parse(response.body);
-  console.log(weatherData.current.temperature);
+request({ url: url, json: true }, (error, response) => {
+  const weatherData = response.body;
+  console.log(weatherData);
 });
