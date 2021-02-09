929865401489c93ba51041b25801cb9d1c7282af
suyashmisra <suyash.misra@quovantis.com>
2021-02-10 01:52:35 +0530

    changed unit to fahrenheit and logged temperature

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require("request");
 const url =
-  "http://api.weatherstack.com/current?access_key=a6339f0a06cad65af649e579b9debaeb&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=a6339f0a06cad65af649e579b9debaeb&query=37.8267,-122.4233&unit=f"; //changed unit to fahrenheit
 
 request({ url: url, json: true }, (error, response) => {
-  const weatherData = response.body;
-  console.log(weatherData);
+  const weatherData = response.body.current;
+  //   console.log(weatherData);
+
+  console.log(
+    `${weatherData.weather_descriptions[0]}. It is currently ${weatherData.temperature} fahrenheit out. It feels like ${weatherData.feelslike} fahrenheit out`
+  );
 });
